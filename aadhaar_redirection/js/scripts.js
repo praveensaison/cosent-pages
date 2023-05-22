f04434e374e179d0b41616d78452646b12b1eca9
Atul <atul.gupta@creditsaison-in.com>
2023-05-22 07:39:45 +0530

    commit

--- aadhaar_redirection/js/scripts.js
+++ aadhaar_redirection/js/scripts.js
@@ -115,6 +115,37 @@ function generateOtp() {
         });
 }
 
+
+const inputs = document.querySelectorAll(".otp-field input");
+inputs.forEach((input, index) => {
+    input.dataset.index = index;
+    input.addEventListener("keyup", handleOtp);
+    input.addEventListener("paste", handleOnPasteOtp);
+});
+
+function handleOtp(e) {
+    const input = e.target;
+    let value = input.value;
+    let isValidInput = value.match(/[0-9a-z]/gi);
+    input.value = "";
+    input.value = isValidInput ? value[0] : "";
+    let fieldIndex = input.dataset.index;
+    if (fieldIndex < inputs.length - 1 && isValidInput) {
+        input.nextElementSibling.focus();
+    }
+    if (e.key === "Backspace" && fieldIndex > 0) {
+        input.previousElementSibling.focus();
+    }
+}
+
+function handleOnPasteOtp(e) {
+    const data = e.clipboardData.getData("text");
+    const value = data.split("");
+    if (value.length === inputs.length) {
+        inputs.forEach((input, index) => (input.value = value[index]));
+    }
+}
+
 function submitOTP() {
     console.log("submitOTP");
     var otpInputs = document.querySelectorAll(".otp-field input");
@@ -154,34 +185,4 @@ function submitOTP() {
             // handle the error
             window.location.href = errorPage;
         });
-}
-
-const inputs = document.querySelectorAll(".otp-field input");
-inputs.forEach((input, index) => {
-    input.dataset.index = index;
-    input.addEventListener("keyup", handleOtp);
-    input.addEventListener("paste", handleOnPasteOtp);
-});
-
-function handleOtp(e) {
-    const input = e.target;
-    let value = input.value;
-    let isValidInput = value.match(/[0-9a-z]/gi);
-    input.value = "";
-    input.value = isValidInput ? value[0] : "";
-    let fieldIndex = input.dataset.index;
-    if (fieldIndex < inputs.length - 1 && isValidInput) {
-        input.nextElementSibling.focus();
-    }
-    if (e.key === "Backspace" && fieldIndex > 0) {
-        input.previousElementSibling.focus();
-    }
-}
-
-function handleOnPasteOtp(e) {
-    const data = e.clipboardData.getData("text");
-    const value = data.split("");
-    if (value.length === inputs.length) {
-        inputs.forEach((input, index) => (input.value = value[index]));
-    }
 }
