54c86a6a40be8eb5d73f1602adbc8404616a61fc
Shivam Agarwalla <shivam.agarwalla@creditsaison-in.com>
2023-07-17 16:05:20 +0530

    CLS-38405: Edit redirect time

--- aadhaar_redirection/static/js/redirect.js
+++ aadhaar_redirection/static/js/redirect.js
@@ -1,7 +1,11 @@
 let url = new URL(window.location.href);
 const callbackUrl = url.searchParams.get("callbackUrl");
 
-setTimeout(redirectUrl, 5000);
+if (url.pathname === "/pages/ThankYou.html") {
+    setTimeout(redirectUrl, 2000);
+} else {
+    setTimeout(redirectUrl, 4000);
+}
 
 function redirectUrl() {
     if (callbackUrl != null) {
