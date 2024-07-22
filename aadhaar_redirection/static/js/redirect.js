4601f358429049a3d7ae3edca5da06450c4681b7
Shivam Agarwalla <shivam.agarwalla@creditsaison-in.com>
2024-07-22 20:04:36 +0530

    PCPE-2847: Fix redirect

--- aadhaar_redirection/static/js/redirect.js
+++ aadhaar_redirection/static/js/redirect.js
@@ -2,4 +2,4 @@ let url = new URL(window.location.href);
 const token = url.searchParams.get("token");
 const callbackUrl = getCallbackUrlFromToken()
 
-// setTimeout(redirectUrl, 4000);
+setTimeout(redirectUrl, 4000);
